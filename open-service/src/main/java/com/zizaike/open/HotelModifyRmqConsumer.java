fcdd86639bd08f4cf61050e239e0cc1226f93802
chenyuqun <346573209@qq.com>
2016-01-26 10:12:35 +0800

    消费者

--- open-service/src/main/java/com/zizaike/open/HotelModifyRmqConsumer.java
+++ open-service/src/main/java/com/zizaike/open/HotelModifyRmqConsumer.java
@@ -9,8 +9,14 @@
   
 package com.zizaike.open;  
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.taobao.api.ApiException;
+import com.taobao.api.DefaultTaobaoClient;
+import com.taobao.api.TaobaoClient;
+import com.taobao.api.request.XhotelUpdateRequest;
+import com.taobao.api.response.XhotelUpdateResponse;
 import com.zizaike.entity.open.alibaba.Hotel;
 
 
@@ -25,7 +31,31 @@ import com.zizaike.entity.open.alibaba.Hotel;
  */
 @Service("hotelModifyRmqConsumer")
 public class HotelModifyRmqConsumer {
-    public void reveiveHotelModifyMessage(Hotel object){
+    @Value("${alibaba.appkey}")
+    private String appkey;
+    @Value("${alibaba.sessionKey}")
+    private String sessionKey;
+    @Value("${alibaba.secret}")
+    private String secret;   
+    private String url="http://gw.api.tbsandbox.com/router/rest";
+    TaobaoClient client = new DefaultTaobaoClient(url, appkey, secret);
+    public void reveiveHotelModifyMessage(Hotel object) throws ApiException{
+        XhotelUpdateRequest req = new XhotelUpdateRequest();
+        
+        req.setOuterId(object.getOuterId());
+        req.setName(object.getName());
+        req.setCity(object.getCity());
+        req.setAddress(object.getAddress());
+        req.setLatitude(object.getLatitude());
+        req.setLongitude(object.getLongitude());
+        req.setPositionType(object.getPositionType());
+        req.setTel(object.getTel());
+//        req.setService("{\"receiveForeignGuests\":\"true\",\"morningCall\":\"true\",\"breakfast\":\"true\"}");
+//        req.setHotelPolicies("{\"children_age_from\":\"2\",\"children_age_to\":\"3\",\"children_stay_free\":\"True\",\"infant_age\":\"1\",\"min_guest_age\":\"4\"}");
+        TaobaoClient client = new DefaultTaobaoClient(url, appkey, secret);
+        XhotelUpdateResponse response = client.execute(req , sessionKey);
+        System.out.println("test");   
+        System.out.println(response.getBody());       
         System.err.println("reveiveOrderModifyMessage"+object);
     }
 }
--- open-service/src/test/java/com/zizaike/open/service/RabbitMqTest.java
+++ open-service/src/test/java/com/zizaike/open/service/RabbitMqTest.java
@@ -14,6 +14,10 @@ import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.annotations.Test;
 
+import com.taobao.api.DefaultTaobaoClient;
+import com.taobao.api.TaobaoClient;
+import com.taobao.api.request.XhotelUpdateRequest;
+import com.taobao.api.response.XhotelUpdateResponse;
 import com.zizaike.core.framework.exception.ZZKServiceException;
 import com.zizaike.entity.open.alibaba.Action;
 import com.zizaike.entity.open.alibaba.Hotel;
@@ -25,12 +29,20 @@ public class RabbitMqTest extends BaseTest {
     AmqpTemplate modifyHotelTemplate;
     @Autowired
     AmqpTemplate modifyRoomTypeTemplate;
-
+    XhotelUpdateRequest req = new XhotelUpdateRequest();
+   
     @Test(description = "rabbitmq hotel convertAndSend 测试")
     public void hotelConvertAndSend() throws ZZKServiceException, InterruptedException {
             Hotel hotel = new Hotel();
             hotel.setAction(Action.ADD);
-            hotel.setAddress("中国 上海,测试");
+            hotel.setOuterId("12345678");
+            hotel.setName("自在客总部");
+            hotel.setCity((long) 310100);
+            hotel.setAddress("浦东金科路");
+            hotel.setLatitude("31.20624");
+            hotel.setLongitude("121.60190");
+            hotel.setPositionType("G");
+            hotel.setTel("4008886232");
             modifyHotelTemplate.convertAndSend(hotel);
     }
     @Test(description = "rabbitmq roomType convertAndSend 测试")
