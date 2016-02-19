8140fdae51dc6f8392ea1cb60823e4da4a3e5e98
snowzhang <snowzhang@zizaike.com>
2016-02-19 15:52:41 +0800

    change order

---
+++ open-service/src/main/java/com/zizaike/open/gateway/OrderService.java
@@ -0,0 +1,23 @@
+/**  
+ * Project Name:open-service  <br/>
+ * File Name:OrderService.java  <br/>
+ * Package Name:com.zizaike.open.gateway  <br/>
+ * Date:2016年2月19日上午11:36:00  <br/>
+ * Copyright (c) 2016, zizaike.com All Rights Reserved.  
+ *  
+*/  
+  
+package com.zizaike.open.gateway;  
+/**  
+ * ClassName:OrderService <br/>  
+ * Function:. <br/>  
+ * Date:     2016年2月19日 上午11:36:00 <br/>  
+ * @author   snow.zhang  
+ * @version    
+ * @since    JDK 1.7  
+ * @see        
+ */
+public interface OrderService {
+    
+}
+  
