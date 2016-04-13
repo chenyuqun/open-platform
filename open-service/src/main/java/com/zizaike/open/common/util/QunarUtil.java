ae7d492a4cb597e28ceec58f027558766f3cb803
chenyuqun <346573209@qq.com>
2016-04-13 17:26:09 +0800

    价格基础信息

---
+++ open-service/src/main/java/com/zizaike/open/common/util/QunarUtil.java
@@ -0,0 +1,147 @@
+package com.zizaike.open.common.util;
+
+import com.zizaike.entity.open.qunar.response.HotelList;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Project Name: code <br/>
+ * Function:QunarListUtil. <br/>
+ * date: 2016/4/12 14:11 <br/>
+ *
+ * @author alex
+ * @since JDK 1.7
+ */
+public class QunarUtil {
+    public static List<String> stringToList(String args) {
+        String[] result = args.split("\\|");
+        List<String> resultList = new ArrayList<String>();
+        for (String arg : result) {
+            resultList.add(arg);
+        }
+        return resultList;
+    }
+
+    public static <T> String listToString(List<T> params) {
+        if (params == null) {
+            return null;
+        }
+        StringBuilder result = new StringBuilder();
+        boolean flag = false;
+        for (T t : params) {
+            if (flag) {
+                result.append("|");
+            } else {
+                flag = true;
+            }
+            result.append(t);
+        }
+        return result.toString();
+    }
+
+    public static <T> String repeatToString(T param, Integer times) {
+        if (times < 0) {
+            return null;
+        } else {
+            StringBuilder result = new StringBuilder();
+            boolean flag = false;
+            while (times > 0) {
+                if (flag) {
+                    result.append("|" + param);
+                } else {
+                    flag = true;
+                    result.append(param);
+                }
+                times--;
+            }
+            return result.toString();
+        }
+    }
+
+    public static String StandardPhoneUtil(String phone) {
+        String standardPhone = null;
+        if (phone == null || phone == "" || phone.equals(" , "))
+            return null;
+        String regNumber = "[^0-9]";
+        String regEmail = "^([a-z0-9A-Z]+[-|_|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
+        Pattern patternEmail = Pattern.compile(regEmail);
+        Matcher matcherEmail = patternEmail.matcher(phone);
+        if (phone.length() < 20) {
+            Pattern patternNum = Pattern.compile(regNumber);
+            Matcher matcherNum = patternNum.matcher(phone);
+            standardPhone = matcherNum.replaceAll("");
+        } else if (matcherEmail.matches()) {
+            return phone;
+        } else {
+            //去除开头是空格的特例
+            if (phone.startsWith(" ")) {
+                StringBuffer stringBufferphone = new StringBuffer(phone);
+                stringBufferphone.replace(0, 1, "");
+                phone = stringBufferphone.toString();
+            }
+            String reg = "（|．|／|～|客|微|或|;|；|/|、|,|，|\\s+";
+            String[] newPhone = phone.split(reg);
+            standardPhone = newPhone[0];
+            Pattern patternNum = Pattern.compile(regNumber);
+            Matcher matcherNum = patternNum.matcher(standardPhone);
+            standardPhone = matcherNum.replaceAll("");
+        }
+        /**
+         * 判断是否是00886，886开头
+         */
+        StringBuffer stringBuffer = new StringBuffer(standardPhone);
+        if (standardPhone.startsWith("00886")) {
+            stringBuffer.insert(5, "-");
+        } else if (standardPhone.startsWith("886")) {
+            stringBuffer.insert(0, "00");
+            stringBuffer.insert(5, "-");
+        } else {
+            stringBuffer.insert(0, "00886-");
+        }
+        standardPhone = stringBuffer.toString();
+        return standardPhone;
+    }
+
+    public static HotelList CoverPhoneNumber(HotelList hotelList) {
+        for (int i = 0; i < hotelList.getHotel().size(); i++) {
+            hotelList.getHotel().get(i).setTel(QunarUtil.StandardPhoneUtil(hotelList.getHotel().get(i).getTel()));
+            ;
+        }
+        return hotelList;
+
+    }
+
+    public static int dateDiff(String checkIn,String checkOut){
+        try{
+            SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+            Calendar cal=Calendar.getInstance();
+            cal.setTime(sdf.parse(checkIn));
+            long time1 = cal.getTimeInMillis();
+            cal.setTime(sdf.parse(checkOut));
+            long time2 = cal.getTimeInMillis();
+            long time=(time2-time1)/(1000*3600*24);
+            return Integer.parseInt(String.valueOf(time));
+        }catch (ParseException e){
+            return 0;
+        }
+
+    }
+    public static void main(String[] args) {
+        String a = "1|2|3|4|5|6|7";
+        List<String> c = QunarUtil.stringToList(a);
+        System.out.println(QunarUtil.stringToList(a));
+        List<Integer> b = new ArrayList<Integer>();
+        b.add(1);
+        b.add(2);
+        b.add(3);
+        System.out.println(QunarUtil.listToString(b));
+        System.out.println(QunarUtil.repeatToString(7, 3));
+        System.out.println("~~~~");
+    }
+}
---
+++ open-service/src/main/java/com/zizaike/open/service/impl/QunarServiceImpl.java
@@ -0,0 +1,310 @@
+
+package com.zizaike.open.service.impl;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.zizaike.core.framework.exception.ZZKServiceException;
+import com.zizaike.entity.open.HomestayDocking;
+import com.zizaike.entity.open.QunarRoomInfoDto;
+import com.zizaike.entity.open.RoomInfoDto;
+import com.zizaike.entity.open.qunar.HotelExt;
+import com.zizaike.entity.open.qunar.request.PriceRequest;
+import com.zizaike.entity.open.qunar.response.*;
+import com.zizaike.is.open.BaseInfoService;
+import com.zizaike.is.open.QunarService;
+import com.zizaike.open.common.util.QunarPhoneUtil;
+import com.zizaike.open.common.util.QunarUtil;
+import com.zizaike.open.common.util.XstreamUtil;
+import com.zizaike.open.dao.HomestayDockingDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Project Name: code <br/>
+ * Function:QunarServiceImpl. <br/>
+ * date: 2016/3/31 14:06 <br/>
+ *
+ * @author alex
+ * @since JDK 1.7
+ */
+@Service
+public class QunarServiceImpl implements QunarService {
+    protected final Logger LOG = LoggerFactory.getLogger(QunarServiceImpl.class);
+    @Autowired
+    private HomestayDockingDao homestayDockingDao;
+    @Autowired
+    private BaseInfoService baseInfoService;
+
+    @Override
+    public String getHotelList() {
+        HotelList hotelList = new HotelList();
+        hotelList.setHotel(homestayDockingDao.queryAllQunarHotel());
+        hotelList = QunarUtil.CoverPhoneNumber(hotelList);
+        String xml = XstreamUtil.getResponseXml(hotelList);
+        return xml;
+    }
+
+    @Override
+    public List<HomestayDocking> queryAll() {
+        return homestayDockingDao.queryAll();
+    }
+
+    @Override
+    public String getPriceResponse(String xml) {
+
+        try {
+            PriceResponse priceResponse=new PriceResponse();
+            SimpleDateFormat sdf = new SimpleDateFormat("YYYY-mm-dd");
+            PriceRequest priceRequest = (PriceRequest) XstreamUtil.getXml2Bean(xml, PriceRequest.class);
+            String hotelId = priceRequest.getHotelId();
+            HotelExt hotelExt = homestayDockingDao.queryQunarHotel(hotelId);
+            QunarPhoneUtil qunarPhoneUtil = new QunarPhoneUtil();
+            String checkIn = priceRequest.getCheckin();
+            String checkOut = priceRequest.getCheckout();
+
+            priceResponse.setHotelId(hotelId);
+            priceResponse.setHotelName(hotelExt.getName());
+            priceResponse.setHotelAddress(hotelExt.getAddress());
+            priceResponse.setHotelCity(hotelExt.getCity());
+            priceResponse.setHotelPhone(qunarPhoneUtil.StandardPhoneUtil(hotelExt.getTel()));
+            priceResponse.setCheckin(checkIn);
+            priceResponse.setCheckout(checkOut);
+            priceResponse.setCurrrencyCode("CNY");
+            //TODO 加入roomList
+            /**
+             * 房间列表
+             */
+            List<Room> roomList = new ArrayList<Room>();
+            String roomId = priceRequest.getRoomId();
+            if (StringUtils.isEmpty(roomId)) {
+                String[] rids = hotelExt.getRids().split(",");
+                for (String rid : rids) {
+                    roomList.add(this.getRoomPriceResponse(rid, checkIn, checkOut));
+                }
+            } else {
+                roomList.add(this.getRoomPriceResponse(roomId, checkIn, checkOut));
+            }
+            priceResponse.setRooms(roomList);
+            String priceResponeXml = XstreamUtil.getResponseXml(priceResponse);
+            return priceResponeXml;
+        } catch (Exception e) {
+            LOG.error("getPriceResponse exception{}",e);
+            PriceResponse priceResponse=new PriceResponse();
+            String priceResponeXml = XstreamUtil.getResponseXml(priceResponse);
+            return priceResponeXml;
+        }
+
+    }
+
+    public Room getRoomPriceResponse(String roomId, String checkIn, String checkOut) {
+        Room room = new Room();
+        /**
+         * 订单填写时才需要
+         */
+        try {
+            QunarRoomInfoDto qunarRoomInfo = baseInfoService.getQunarRoomInfo(Integer.valueOf(roomId));
+            int dateDiff = QunarUtil.dateDiff(checkIn, checkOut);
+            /**
+             * 房态房价
+             */
+            JSONObject result = baseInfoService.getZizaikePrice(roomId, checkIn, checkOut);
+            if (result.getString("status").equals("ok")) {
+                int roomStyle = result.getJSONObject("response").getIntValue("room_style");
+                JSONArray jsonArray = result.getJSONObject("response").getJSONArray("list");
+                List<Integer> priceCNList = new ArrayList<Integer>();
+                List<Integer> numList = new ArrayList<Integer>();
+                List<String> statusList = new ArrayList<String>();
+                for (int i = 0; i < jsonArray.size(); i++) {
+                    //取人民币
+                    int priceCN = jsonArray.getJSONObject(i).getIntValue("price_cn");
+                    priceCNList.add(priceCN);
+                    //房间数
+                    int num = jsonArray.getJSONObject(i).getIntValue("num");
+                    numList.add(num);
+                    if (num > 0) {
+                        String status = "ACTIVE";
+                        statusList.add(status);
+                    } else {
+                        String status = "DISABLED";
+                        statusList.add(status);
+                    }
+                }
+                room.setId(roomId);
+//                room.setArea("");
+                room.setBroadband(FeeMode.UNKNOWN.getCode());
+                room.setCheckinTime(qunarRoomInfo.getCheckinAt());
+                room.setCheckoutTime(qunarRoomInfo.getCheckoutAt());
+                room.setCounts(QunarUtil.listToString(numList));
+                room.setStatus(QunarUtil.listToString(statusList));
+//            room.setFreeChildrenAgeLimit(8);
+//            room.setFreeChildrenNumber(1);
+                room.setInstantConfirmRoomCount(QunarUtil.listToString(numList));
+                room.setPrices(QunarUtil.listToString(priceCNList));
+                room.setName(qunarRoomInfo.getTitle());
+                int maxOccupancy = 2;
+                if (qunarRoomInfo.getValue() == 1) {
+                    //有早餐 取人数
+                    if (qunarRoomInfo.getName().equals("10+")) {
+                        maxOccupancy = 10;
+                    } else {
+                        maxOccupancy = Integer.parseInt(qunarRoomInfo.getName());
+                    }
+                }
+
+                room.setMaxOccupancy(maxOccupancy);
+                if (roomStyle == 2) {
+                    room.setOccupancyNumber(1);
+                } else {
+                    room.setOccupancyNumber(room.getMaxOccupancy());
+                }
+
+                room.setPayType(PayType.PREPAY);
+                room.setRoomRate(QunarUtil.listToString(priceCNList));
+                if (qunarRoomInfo.getWifi() == 1) {
+                    room.setWifi(FeeMode.FREE.getCode());
+                } else {
+                    room.setWifi(FeeMode.NONE.getCode());
+                }
+
+                room.setTaxAndFee(QunarUtil.repeatToString(0, dateDiff));
+            } else {
+                return room;
+            }
+            /**
+             * 床型
+             */
+            BedType bedType = new BedType();
+            //bedType.setRelation("OR");
+            List<Beds> bedsList = new ArrayList<Beds>();
+            Beds beds = new Beds();
+            beds.setSeq("1");
+            /**
+             * count
+             */
+            if (StringUtils.isEmpty(qunarRoomInfo.getFieldCHuangshuTid())) {
+                beds.setCount(2);
+            } else if (qunarRoomInfo.getFieldCHuangshuTid() == 320) {
+                beds.setCount(0);
+            } else if (qunarRoomInfo.getFieldCHuangshuTid() == 309) {
+                beds.setCount(1);
+            } else if (qunarRoomInfo.getFieldCHuangshuTid() == 310) {
+                beds.setCount(2);
+            } else if (qunarRoomInfo.getFieldCHuangshuTid() == 315) {
+                beds.setCount(3);
+            } else if (qunarRoomInfo.getFieldCHuangshuTid() == 313) {
+                beds.setCount(4);
+            } else if (qunarRoomInfo.getFieldCHuangshuTid() == 321) {
+                beds.setCount(5);
+            } else if (qunarRoomInfo.getFieldCHuangshuTid() == 312) {
+                beds.setCount(6);
+            } else if (qunarRoomInfo.getFieldCHuangshuTid() == 322) {
+                beds.setCount(7);
+            } else if (qunarRoomInfo.getFieldCHuangshuTid() == 311) {
+                beds.setCount(8);
+            } else if (qunarRoomInfo.getFieldCHuangshuTid() == 323) {
+                beds.setCount(9);
+            } else if (qunarRoomInfo.getFieldCHuangshuTid() == 314) {
+                beds.setCount(10);
+            }
+            /**
+             * 床型
+             */
+            if (StringUtils.isEmpty(qunarRoomInfo.getFieldChuangxingTid())) {
+                beds.setCode(BedTypeCode.OTHER);
+                beds.setDesc(BedTypeCode.getByCode("OTHER"));
+            } else if (qunarRoomInfo.getFieldChuangxingTid() == 316) {
+                beds.setCount(2);
+                beds.setCode(BedTypeCode.SINGLE);
+                beds.setDesc(BedTypeCode.getByCode("SINGLE"));
+            } else if (qunarRoomInfo.getFieldChuangxingTid() == 324) {
+                beds.setCode(BedTypeCode.DORM_BED);
+                beds.setDesc(BedTypeCode.getByCode("DORM_BED"));
+            } else if (qunarRoomInfo.getFieldChuangxingTid() == 318) {
+                beds.setCode(BedTypeCode.TATAMI);
+                beds.setDesc(BedTypeCode.getByCode("TATAMI"));
+            } else if (qunarRoomInfo.getFieldChuangxingTid() == 317) {
+                beds.setCode(BedTypeCode.SINGLE);
+                beds.setDesc(BedTypeCode.getByCode("SINGLE"));
+            } else if (qunarRoomInfo.getFieldChuangxingTid() == 325) {
+                beds.setCode(BedTypeCode.BUNK);
+                beds.setDesc(BedTypeCode.getByCode("BUNK"));
+            }
+            // beds.setSize("1.8m*2m");
+            bedsList.add(beds);
+            bedType.setBeds(bedsList);
+            room.setBedType(bedType);
+            /**
+             * meal节点
+             */
+            Meal meal = new Meal();
+            int breakfastNum = 0;
+            if (qunarRoomInfo.getValue() == 1) {
+                if (qunarRoomInfo.getName().equals("10+")) {
+                    breakfastNum = 10;
+                } else {
+                    breakfastNum = Integer.parseInt(qunarRoomInfo.getName());
+                }
+            }
+            String breakfast = QunarUtil.repeatToString(breakfastNum, dateDiff);
+            String lunch = QunarUtil.repeatToString(0, dateDiff);
+            meal.setBreakfast(new Breakfast(breakfast, ""));
+            meal.setLunch(new Lunch(lunch, ""));
+            meal.setDinner(new Dinner(lunch, ""));
+            room.setMeal(meal);
+            /**
+             * 退款政策
+             */
+            List<RefundRule> refundRules = new ArrayList<RefundRule>();
+            List<NonRefundableRange> nonRefundableRanges = new ArrayList<NonRefundableRange>();
+
+            if (org.apache.commons.lang.StringUtils.isEmpty(qunarRoomInfo.getRefundRule())) {
+                refundRules.add(new RefundRule(720, RefundType.DEDUCT_BY_PERCENT, "0"));
+                refundRules.add(new RefundRule(0, RefundType.DEDUCT_BY_PERCENT, "100"));
+            } else {
+                JSONObject refundRule = JSON.parseObject(qunarRoomInfo.getRefundRule());
+                if (refundRule.get("type").equals(1)) {
+                    JSONArray refundList = refundRule.getJSONArray("refund_list");
+                    Boolean firstRefund = true;
+                    for (int i = 1; i <= refundList.size(); i++) {
+                        if (firstRefund) {
+                            refundRules.add(new RefundRule(refundList.getJSONObject(i).getIntValue("day"), RefundType.DEDUCT_BY_PERCENT, "0"));
+                            firstRefund = false;
+                        } else {
+                            refundRules.add(new RefundRule(refundList.getJSONObject(i).getIntValue("day"), RefundType.DEDUCT_BY_PERCENT, refundList.getJSONObject(i).getString("per")));
+                        }
+                    }
+                    refundRules.add(new RefundRule(0, RefundType.DEDUCT_BY_PERCENT, "100"));
+                } else {
+                    nonRefundableRanges.add(new NonRefundableRange(checkIn, checkOut));
+                }
+            }
+            Refund refund = new Refund();
+            refund.setReturnable(Boolean.TRUE);
+            refund.setTimeZone("GMT+8");
+            refund.setRefundRules(refundRules);
+            refund.setNonRefundableRanges(nonRefundableRanges);
+            room.setRefund(refund);
+            /**
+             * 可选节点
+             */
+//            room.setRemarks(remarkList);
+//            room.setOptionRules(optionRuleList);
+//            room.setPromotionRules(promotionRuleList);
+//            room.setExtras(extraList);
+
+        } catch (ZZKServiceException e) {
+            LOG.error("getRoomPriceResponse exception{}",e);
+        }
+
+
+        return room;
+    }
+}
